dd4b17a5eb78fc87854e9bcbc4b5dc0222cb7f5f
Jin Siang Ong <jong0038@student.monash.edu>
2021-10-03 12:37:58 +0800

    modified scrollbar to have detection

--- ball_detection/ball_detection.py
+++ ball_detection/ball_detection.py
@@ -1,25 +1,11 @@
-# import cv2
-# import numpy as np
-#
-# img = cv2.imread('group 4/frame000041.png')
-# dimensions = img.shape
-# print("Image Size:" + str(dimensions))
-#
-# blur_img = cv2.GaussianBlur(img, (7, 7), cv2.BORDER_DEFAULT)
-# hsv_img = cv2.cvtColor(blur_img, cv2.COLOR_BGR2HSV)
-# low = np.array([,20])
-#
-# cv2.imshow("Original", gray_img)
-# cv2.waitKey(0)
-
 import cv2
 import numpy as np
 
 def nothing(x):
     pass
 
-# Load image
-image = cv2.imread('group 4/frame000000.png')
+# Load img
+img = cv2.imread('group 4/frame000000.png')
 
 # Create a window
 cv2.namedWindow('image')
@@ -56,9 +42,9 @@ while(1):
     upper = np.array([hMax, sMax, vMax])
 
     # Convert to HSV format and color threshold
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower, upper)
-    result = cv2.bitwise_and(image, image, mask=mask)
+    result = cv2.bitwise_and(img, img, mask=mask)
 
     # Print if there is a change in HSV value
     if((phMin != hMin) | (psMin != sMin) | (pvMin != vMin) | (phMax != hMax) | (psMax != sMax) | (pvMax != vMax) ):
@@ -70,67 +56,56 @@ while(1):
         psMax = sMax
         pvMax = vMax
 
+    gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+
+    # hough circles
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 80, param1=50, param2=20, minRadius=10, maxRadius=25)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+    img_circles = img.copy()
+
+    if circles is not None:
+        for i in circles[0, :]:
+            # draw the outer circle
+            cv2.circle(img_circles, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            # draw the center of the circle
+            cv2.circle(img_circles, (i[0], i[1]), 2, (0, 0, 255), 3)
+
     # Display result image
-    cv2.imshow('image', result)
+    cv2.imshow('image', img_circles)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
 cv2.destroyAllWindows()
 
+# img = cv2.imread('group 4/frame000002.png')
+# dimensions = img.shape
+# print("Image Size:" + str(dimensions))
 #
-# # adaptive threshold
-# thresh1 = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-# contours1, ret = cv2.findContours(thresh1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+# hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+# lower = np.array([0, 0, 0])
+# upper = np.array([179, 57, 137])
+# mask = cv2.inRange(hsv, lower, upper)
+# result = cv2.bitwise_and(img, img, mask=mask)
 #
-# img_contour1 = img.copy()
-# cv2.drawContours(image=img_contour1, contours=contours1, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+# cv2.imshow("Mask Result", result)
 #
-# circles1 = cv2.HoughCircles(thresh1, cv2.HOUGH_GRADIENT, 1, 80, param1=50, param2=20, minRadius=0, maxRadius=0)
-# circles1 = np.uint16(np.around(circles1))
-# img_circles1 = img.copy()
+# gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
 #
-# if circles1 is not None:
-#     for i in circles1[0, :]:
+# # hough circles
+# circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 80, param1=50, param2=20, minRadius=10, maxRadius=25)
+# if circles is not None:
+#     circles = np.uint16(np.around(circles))
+# img_circles = img.copy()
+#
+# if circles is not None:
+#     for i in circles[0, :]:
 #         # draw the outer circle
-#         cv2.circle(img_circles1, (i[0], i[1]), i[2], (0, 255, 0), 2)
+#         cv2.circle(img_circles, (i[0], i[1]), i[2], (0, 255, 0), 2)
 #         # draw the center of the circle
-#         cv2.circle(img_circles1, (i[0], i[1]), 2, (0, 0, 255), 3)
-#
-# # multiple object detection
-#
-# ret, thresh2 = cv2.threshold(gray_img,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-# # noise removal
-# kernel = np.ones((3,3),np.uint8)
-# opening = cv2.morphologyEx(thresh2,cv2.MORPH_OPEN,kernel, iterations = 2)
-# # sure background area
-# sure_bg = cv2.dilate(opening,kernel,iterations=3)
-# # Finding sure foreground area
-# dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
-# ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
-# # Finding unknown region
-# sure_fg = np.uint8(sure_fg)
-# unknown = cv2.subtract(sure_bg,sure_fg)
-#
-# # Marker labelling
-# ret, markers = cv2.connectedComponents(sure_fg)
-# # Add one to all labels so that sure background is not 0, but 1
-# markers = markers+1
-# # Now, mark the region of unknown with zero
-# markers[unknown==255] = 0
-#
-# img_multiple = img.copy()
-# markers = cv2.watershed(img_multiple,markers)
-# img_multiple[markers == -1] = [255,0,0]
+#         cv2.circle(img_circles, (i[0], i[1]), 2, (0, 0, 255), 3)
 #
 # # showing images
-#
-# numpy_horizontal1 = np.hstack((cv2.cvtColor(thresh1, cv2.COLOR_GRAY2BGR), img_contour1, img_circles1))
-# # numpy_horizontal = np.hstack((img_contour, cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)))
-# # numpy_horizontal1 = np.hstack((img_contour1, img_circles1))
-#
 # # cv2.imshow("Original", img)
-# cv2.imshow("Adaptive Result", numpy_horizontal1)
-# # cv2.imshow("Manual Result", img_contour)
-# # cv2.imshow("Adaptive Result", img_contour1)
-# cv2.imshow("Multiple Object", img_multiple)
+# cv2.imshow("Hough Circle Result", img_circles)
 # cv2.waitKey(0)
