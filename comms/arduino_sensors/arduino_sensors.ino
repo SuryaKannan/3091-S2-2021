f17ffd2ccddf8960f4b9d8ff80e667b0febd54d0
Kurt <kb7737@gmail.com>
2021-10-03 15:34:03 +1100

    Added servo functionality

--- comms/arduino_sensors/arduino_sensors.ino
+++ comms/arduino_sensors/arduino_sensors.ino
@@ -5,20 +5,20 @@
  * - NewPing
  * 
  * 1) Sends ultrasonic sensor information from Arduino to Jetson Nano via i2c. Up to four ultrasonics are supported (follow pin layout below)
- * 
- * TODO:
- * - Servo control 
+ * 2) Sends servo speed once at void setup, servo maintains that speed
  */
  
 #include <NewPing.h>
 #include <Wire.h>
 #include <EnableInterrupt.h>
+#include <Servo.h>
 
 #define ADDRESS 0x8 // i2c address used 
 #define TRIGGER_PIN  5  // Common Trig pin for ultrasonics 
 #define ECHO_PIN_F  8  // front ultrasonic
 #define ECHO_PIN_L  6  // left ultrasonic
 #define ECHO_PIN_R  7  // right ultrasonic
+#define SERVO_PIN 9 // digital pin 9
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 NewPing sonarF(TRIGGER_PIN, ECHO_PIN_F, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
@@ -29,10 +29,19 @@ int distF = 0;
 int distL = 0;
 int distR = 0;
 
+Servo paddleServo;
+int servoSpeed = 70; // global var for servo speed, 70 appears to be the magic number
+
 // set up Serial, pins and I2C
 void setup() {
   Wire.begin(ADDRESS);
   Wire.onRequest(requestEvent);
+
+  paddleServo.attach(SERVO_PIN);
+  paddleServo.write(servoSpeed);
+  Serial.print("Initial servo speed: ");
+  Serial.println(servoSpeed);
+
   Serial.begin(115200);
 }
 
@@ -51,5 +60,16 @@ void requestEvent() {
   sensor_data[1] = distL;
   sensor_data[2] = distR;
   Wire.write((byte *) sensor_data, sizeof sensor_data);
+
+  Serial.print("F: ");
+  Serial.print(distF);
+  Serial.print('\t');
+
+  Serial.print("L: ");
+  Serial.print(distL);
+  Serial.print('\t');
+
+  Serial.print("R: ");
+  Serial.println(distR);
   
 }
