4ced1878e7079b7cee133be195a9cd90a033c4a1
Jin Siang Ong <jong0038@student.monash.edu>
2021-10-16 18:59:29 +0800

    added lights

--- ball_detection/ball_detection.py
+++ ball_detection/ball_detection.py
@@ -5,10 +5,12 @@ def nothing(x):
     pass
 
 # Load img
-# video = cv2.VideoCapture('Data/video.avi')
-images = ['distractor.jpg', 'distractor_target.jpg', 'distractor_target_obstacle.jpg', 'no_balls.jpg', 'obstacle_distractor.jpg', 'target_bearing_no_obstacle.jpg', 'target_bearing_bostacle.jpg']
-target_img = images[5]
-img = cv2.imread(target_img)
+video = cv2.VideoCapture('vid2_lighter/vid2_lighter.avi')
+# images = ['distractor.jpg', 'distractor_target.jpg', 'distractor_target_obstacle.jpg', 'no_balls.jpg', 'obstacle_distractor.jpg', 'target_bearing_no_obstacle.jpg', 'target_bearing_obstacle.jpg', 'about_to_exit_frame.jpg', 'closest.jpg', 'furthest.jpg', 'furthest_2.jpg']
+# target_img = images[8]
+# img = cv2.imread(target_img)
+
+close_range = 100
 
 # Create a window
 cv2.namedWindow('image')
@@ -25,15 +27,22 @@ cv2.createTrackbar('VMax', 'image', 0, 255, nothing)
 cv2.createTrackbar('dp', 'image', 6, 13, nothing)
 cv2.createTrackbar('param1', 'image', 24, 100, nothing)
 cv2.createTrackbar('param2', 'image', 30, 100, nothing)
+cv2.createTrackbar('minRadius', 'image', 1, 100, nothing)
+cv2.createTrackbar('maxRadius', 'image', 1, 100, nothing)
 
 # Set default value for Max HSV trackbars
-cv2.setTrackbarPos('dp', 'image', 11)
-cv2.setTrackbarPos('param1', 'image', 50)
-cv2.setTrackbarPos('param2', 'image', 30)
-
+cv2.setTrackbarPos('HMin', 'image', 0)
+cv2.setTrackbarPos('SMin', 'image', 0)
+cv2.setTrackbarPos('VMin', 'image', 0)
 cv2.setTrackbarPos('HMax', 'image', 179)
-cv2.setTrackbarPos('SMax', 'image', 100)
+cv2.setTrackbarPos('SMax', 'image', 70)
 cv2.setTrackbarPos('VMax', 'image', 255)
+
+cv2.setTrackbarPos('dp', 'image', 10)
+cv2.setTrackbarPos('param1', 'image', 50)
+cv2.setTrackbarPos('param2', 'image', 40)
+cv2.setTrackbarPos('minRadius', 'image', 10)
+cv2.setTrackbarPos('maxRadius', 'image', 60)
 
 # Initialize HSV min/max values
 hMin = sMin = vMin = hMax = sMax = vMax = 0
@@ -52,12 +61,14 @@ while(1):
     dp = cv2.getTrackbarPos('dp', 'image')
     param1 = cv2.getTrackbarPos('param1', 'image')
     param2 = cv2.getTrackbarPos('param2', 'image')
+    minRadius = cv2.getTrackbarPos('minRadius', 'image')
+    maxRadius = cv2.getTrackbarPos('maxRadius', 'image')
 
     # Set minimum and maximum HSV values to display
     lower = np.array([hMin, sMin, vMin])
     upper = np.array([hMax, sMax, vMax])
 
-    # ret, img = video.read()
+    ret, img = video.read()
     dimensions = img.shape
     # print("Image Size:" + str(dimensions))
     height = dimensions[0]
@@ -81,17 +92,16 @@ while(1):
     gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
 
     # hough circles
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, dp/10, 80, param1=param1, param2=param2, minRadius=10, maxRadius=25)
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, dp/10, 80, param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
     if circles is not None:
         circles = np.uint16(np.around(circles))
 
     img_circles = result.copy()
 
-    close_range = 20
     centre_width = int(width/2)
 
     cv2.line(img_circles, (centre_width, 0), (centre_width, height), (255, 0, 0), 2)
-    cv2.line(img_circles, (centre_width, centre_width-close_range), (centre_width, height), (0, 0, 255), 2)
+    cv2.line(img_circles, (centre_width, height-close_range), (centre_width, height), (0, 0, 255), 2)
 
     if circles is not None:
         for i in circles[0, :]:
@@ -101,13 +111,16 @@ while(1):
             cv2.circle(img_circles, (i[0], i[1]), 2, (0, 0, 255), 3)
             distance = i[0]-centre_width
             print(distance)
+            if(i[1]>=(height-close_range)):
+                print("Target in range")
 
     # Display result image
     cv2.imshow('image', img)
-    cv2.imshow(target_img, img_circles)
+    # cv2.imshow(target_img, img_circles)
+    cv2.imshow('result', img_circles)
 
     # Press Q on keyboard to stop recording
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1000) & 0xFF == ord('q'):
         break
 
 cv2.destroyAllWindows()
